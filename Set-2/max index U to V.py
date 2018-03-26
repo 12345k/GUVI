956f7d7be15b02429b5b456e31a98f3cc9d4c498
Karthick Aravindan <karthickaravindan@gmail.com>
2018-03-26 23:28:22 +0530

    Add files via upload

---
+++ Set-2/max index U to V.py
@@ -0,0 +1,5 @@
+n,q= map(int,input().split())
+N=[int(i) for i in input().split()]
+for _ in range(q):
+    u,v=map(int,input().split())
+    print(max(N[u-1:v])) # if we need minimum change max to min
